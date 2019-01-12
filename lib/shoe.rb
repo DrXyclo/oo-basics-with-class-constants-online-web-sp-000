6607d677c0acc6752d1fe2c8714e9fc5cce4d53f
Dr. XyClo <sdcarmody@gmail.com>
2019-01-12 14:33:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
-binding.pry 
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -7,11 +8,34 @@ class Shoe
     @brand = brand
     BRANDS.each do |brands_array|
       brands_array == brand
+      binding.pry
     BRANDS << brand
   end
   end
   
   BRANDS = []
+  
+=begin 
+      it 'keeps track of all brands' do
+      brands = ["Uggs", "Rainbow"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      brands.each do |brand|
+        expect(Shoe::BRANDS).to include(brand)
+      end
+    end
+
+    it 'only keeps track of unique brands' do
+      Shoe::BRANDS.clear
+      brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      expect(Shoe::BRANDS.size).to eq(3)
+=end 
 
   def cobble
     self.condition = "new"
